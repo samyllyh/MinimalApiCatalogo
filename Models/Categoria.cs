ccc88febb6cb0939e2229002ee0e18052d28dbac
Samylly Pires Paulo <samylly.paulo@fitbank.com.br>
2022-12-30 09:55:46 -0300

    criação das entidades

---
+++ Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace MinimalApiCatalogo.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Descricao { get; set; }
+
+        public ICollection<Produto>? Produtos { get; set; }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿namespace MinimalApiCatalogo.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Decricao { get; set; }
+
+        public decimal preco { get; set; }
+
+        public string? ImagemUrl { get; set; }
+
+        public DateTime DataCompra { get; set; }
+
+        public int Estoque { get; set; }
+
+        //relacionamento 1 pra n
+        public int CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }  
+    }
+}
